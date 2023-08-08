f1eb7af97a8213299ddba63aae6fd3204a01c426
LuisHBeck <beck.henrique.luis@gmail.com>
2023-08-08 10:33:59 -0300

    feat: create static files and templates

--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, HttpResponse
+from django.template import loader
 from .models import Product
 
 def index(request):
@@ -20,8 +21,21 @@ def contact(request):
 
 
 def product(request, pk):
-    prod = Product.objects.get(id=pk)
+    # prod = Product.objects.get(id=pk)
+    
+    prod = get_object_or_404(Product, id=pk)
+    
     context = {
         'product': prod
     }
     return render(request, 'product.html', context)
+
+
+def error404(request, exception):
+    template = loader.get_template('404.html')
+    return HttpResponse(content=template.render(), content_type='text/html; charset=utf8', status=404)
+
+
+def error500(request):
+    template = loader.get_template('500.html')
+    return HttpResponse(content=template.render(), content_type='text/html; charset=utf8', status=500)
--- project/settings.py
+++ project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2##fpm0!uktppvp#o#w@j+vh$)=(yod*_ww8pnmx$v8!b)tur!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -121,8 +121,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'templates/static/')]
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGOUT_REDIRECT_URL = 'index'
